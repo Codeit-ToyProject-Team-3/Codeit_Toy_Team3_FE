022b11a1d0872677abe8bdb5b3cf3501b84e0215
DefineXX <keaikim21@naver.com>
2024-09-29 22:56:11 +0900

    chore(setup): sass, styled-components 설치, 경로 별칭 설정 및  글로벌 스타일 가이드 구현 (#1)

---
+++ src/styles/theme/colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+  red: "#FF5757",
+  black: "#282828",
+  white: "#ffffff",
+  gray: {
+    500: "#8D8D8D",
+    400: "#B8B8B8",
+    200: "#DDDDDD",
+
+    100: "#F4F4F4",
+    50: "#FAFAFA",
+  },
+};
---
+++ src/styles/theme/typography.js
@@ -0,0 +1,81 @@
+import { css } from "styled-components";
+
+export const fontStyles = {
+  Header1: css`
+    font-style: normal;
+    font-size: 30px;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -3%;
+  `,
+  Header2: css`
+    font-style: normal;
+    font-size: 24px;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -3%;
+  `,
+  Header3: css`
+    font-style: normal;
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 28px;
+    letter-spacing: -3%;
+  `,
+  Body1: css`
+    font-style: normal;
+    font-size: 18px;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -3%;
+  `,
+  Body2: css`
+    font-style: normal;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: -3%;
+  `,
+  Body3: css`
+    font-style: normal;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -3%;
+  `,
+  Body4: css`
+    font-style: normal;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: -3%;
+  `,
+  Body5: css`
+    font-style: normal;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: -3%;
+  `,
+  Caption1: css`
+    font-style: normal;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -3%;
+  `,
+  Caption2: css`
+    font-style: normal;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: -3%;
+  `,
+  Footnote: css`
+    font-style: normal;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: -3%;
+  `,
+};
