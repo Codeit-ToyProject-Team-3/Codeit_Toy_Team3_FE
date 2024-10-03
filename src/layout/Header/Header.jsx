eebd26442b2b525bf700fe35b80aa9359b68a834
DefineXX <keaikim21@naver.com>
2024-10-03 23:20:54 +0900

    feat(header): 헤더 컴포넌트 내 그룹 만들기 버튼 페이지별 렌더링 유무 구현

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,19 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import { CreateGroupButton, HeaderContainer, MainLogo } from "./Header.styled";
 
 import mainLogo from "@assets/MainLogo.svg";
 
 const Header = () => {
+  const location = useLocation();
+  const noGroupCreateButtonPages = ["/private-access", "/create-group"];
+
   return (
     <>
       <HeaderContainer>
         <Link to="/">
           <MainLogo src={mainLogo} alt="조각집 메인 로고" />
         </Link>
-        <Link to="/create-group">
-          <CreateGroupButton>그룹 만들기</CreateGroupButton>
-        </Link>
+        {noGroupCreateButtonPages.includes(location.pathname) ? null : (
+          <Link to="/create-group">
+            <CreateGroupButton>그룹 만들기</CreateGroupButton>
+          </Link>
+        )}
       </HeaderContainer>
     </>
   );
