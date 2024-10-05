4f5f89bb6885baaf5b445ab35056f6867e7d3c92
DefineXX <keaikim21@naver.com>
2024-10-05 15:32:33 +0900

    feat(MemorySection): 그룹 추억 목록 헤더 구현 (#13)

--- src/pages/GroupDetailPage/GroupDetailPage.jsx
+++ src/pages/GroupDetailPage/GroupDetailPage.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
+
+import {
+  CreateGroupMemoryButton,
+  GroupDetailDivider,
+  GroupDetailPageContainer,
+  GroupMemoryHeader,
+  GroupMemorySectionTitle,
+} from "./GroupDetailPage.styled";
+
 import Header from "@layout/Header/Header";
 
 import GroupDetailCard from "@components/GroupDetailCard/GroupDetailCard";
-import { GroupDetailPageContainer } from "./GroupDetailPage.styled";
+import GroupSearchBar from "@layout/GroupSearchBar/GroupSearchBar";
 
 const GroupDetailPage = () => {
+  const [privacyMemory, setPrivacyMemory] = useState("공개");
+
   return (
     <>
       <Header />
       <GroupDetailPageContainer>
         <GroupDetailCard />
+        <GroupDetailDivider />
+        <GroupMemoryHeader>
+          <GroupMemorySectionTitle>추억 목록</GroupMemorySectionTitle>
+          <CreateGroupMemoryButton>추억 올리기</CreateGroupMemoryButton>
+        </GroupMemoryHeader>
+        <GroupSearchBar
+          privacyCategory={privacyMemory}
+          setPrivacyCategory={setPrivacyMemory}
+        />
       </GroupDetailPageContainer>
     </>
   );
--- src/pages/GroupDetailPage/GroupDetailPage.styled.js
+++ src/pages/GroupDetailPage/GroupDetailPage.styled.js
@@ -1,3 +1,4 @@
+import { colors } from "@styles/theme/colors";
 import styled from "styled-components";
 
 export const GroupDetailPageContainer = styled.div`
@@ -8,4 +9,45 @@ export const GroupDetailPageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+export const GroupDetailDivider = styled.div`
+  width: 100%;
+  height: 1px;
+
+  margin-top: 120px;
+  margin-bottom: 127px;
+
+  background-color: ${colors.gray[200]};
+`;
+
+export const GroupMemorySectionTitle = styled.h3`
+  ${({ theme }) => theme.fontStyles.Header2}
+`;
+
+export const GroupMemoryHeader = styled.div`
+  width: 100%;
+  height: 45px;
+  margin-bottom: 68px;
+
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CreateGroupMemoryButton = styled.button`
+  width: 200px;
+  height: 45px;
+
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  border-radius: 6px;
+  background-color: ${colors.black};
+
+  ${({ theme }) => theme.fontStyles.Caption1}
+  color: ${colors.gray[50]};
 `;
