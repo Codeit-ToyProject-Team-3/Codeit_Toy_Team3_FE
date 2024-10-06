576648fe09b8a2dfaf4cd0041c0776c9a20c6b47
DefineXX <keaikim21@naver.com>
2024-10-06 15:18:37 +0900

    feat(groupMemory): 그룹 내 추억 목록 유무에 따른 컨텐츠 조건부 렌더링 구현 (#13)

--- src/pages/GroupDetailPage/GroupDetailPage.jsx
+++ src/pages/GroupDetailPage/GroupDetailPage.jsx
@@ -1,28 +1,36 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 import {
   CreateGroupMemoryButton,
+  CreateGroupMemoryButtonLarge,
   GroupDetailDivider,
   GroupDetailPageContainer,
   GroupMemoryHeader,
   GroupMemorySectionTitle,
+  NoGroupMemoryContainer,
 } from "./GroupDetailPage.styled";
 
 import Header from "@layout/Header/Header";
-
-import GroupDetailCard from "@components/GroupDetailCard/GroupDetailCard";
 import GroupSearchBar from "@layout/GroupSearchBar/GroupSearchBar";
+import GroupDetailCard from "@components/GroupDetailCard/GroupDetailCard";
 import PublicMemoeryCard from "@components/PublicMemoryCard/PublicMemoryCard";
 import PrivateMemoryCard from "@components/PrivateMemoryCard/PrivateMemoryCard";
+
+import noMemoryIcon from "@assets/no-group-icon.svg";
 
 const GroupDetailPage = () => {
   const [privacyMemory, setPrivacyMemory] = useState("공개");
   const [groupMemoryList, setGroupMemoryList] = useState([]);
-  const [groupMemoryTotalPage, setGroupMemoryTotalPage] = useState([]);
+  const [groupMemoryTotalPage, setGroupMemoryTotalPage] = useState(null);
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Fetching Group Memory Data
   useEffect(() => {
     const fetchGroupMemoryCardInfos = async () => {
+      setIsLoading(true);
       try {
         const response = await axios.get("/data/memoryList.json");
         const fetchedMemoryList = response.data;
@@ -38,6 +46,9 @@ const GroupDetailPage = () => {
         setGroupMemoryTotalPage(fetchedMemoryList.totalPages);
       } catch (error) {
         console.error("Error occured: ", error);
+        setGroupMemoryList([]);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -58,7 +69,22 @@ const GroupDetailPage = () => {
           privacyCategory={privacyMemory}
           setPrivacyCategory={setPrivacyMemory}
         />
-        {privacyMemory === "공개" ? (
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : groupMemoryList === undefined || groupMemoryList?.length === 0 ? (
+          <>
+            <NoGroupMemoryContainer>
+              <img src={noMemoryIcon} alt="No-Memory-Icon" />
+              <h2 className="notice-1">게시된 추억이 없습니다.</h2>
+              <h3 className="notice-2">첫 번째 추억을 올려보세요!</h3>
+            </NoGroupMemoryContainer>
+            <Link to="/create-memory">
+              <CreateGroupMemoryButtonLarge>
+                그룹 만들기
+              </CreateGroupMemoryButtonLarge>
+            </Link>
+          </>
+        ) : privacyMemory === "공개" ? (
           <PublicMemoeryCard
             groupMemoryList={groupMemoryList}
             listTotalPage={groupMemoryTotalPage}
--- src/pages/GroupDetailPage/GroupDetailPage.styled.js
+++ src/pages/GroupDetailPage/GroupDetailPage.styled.js
@@ -51,3 +51,41 @@ export const CreateGroupMemoryButton = styled.button`
   ${({ theme }) => theme.fontStyles.Caption1}
   color: ${colors.gray[50]};
 `;
+
+export const NoGroupMemoryContainer = styled.div`
+  width: 100%;
+  margin-top: 200px;
+  margin-bottom: 180px;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 40px;
+
+  img {
+    width: 100px;
+    height: 100px;
+  }
+
+  .notice-1 {
+    ${({ theme }) => theme.fontStyles.Body1}
+    color: ${colors.gray[500]};
+  }
+
+  .notice-2 {
+    ${({ theme }) => theme.fontStyles.Caption2}
+    color: ${colors.gray[400]};
+  }
+`;
+
+export const CreateGroupMemoryButtonLarge = styled.button`
+  width: 400px;
+  height: 50px;
+  margin-bottom: 264px;
+
+  border-radius: 6px;
+
+  ${({ theme }) => theme.fontStyles.Caption1}
+  background-color: ${colors.black};
+  color: ${colors.gray[50]};
+`;
