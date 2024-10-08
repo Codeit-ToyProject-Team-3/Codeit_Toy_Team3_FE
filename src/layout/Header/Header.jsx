98367a2b1baacc07d4d5de9f138bce098b20f43a
DefineXX <keaikim21@naver.com>
2024-10-09 01:28:27 +0900

    feat(NotFound): 404 NotFound 페이지 구현

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -6,6 +6,7 @@ import mainLogo from "@assets/MainLogo.svg";
 
 const Header = () => {
   const location = useLocation();
+
   const noGroupCreateButtonPages = [
     "/private-access",
     "/create-group",
@@ -14,15 +15,17 @@ const Header = () => {
     "/posts",
   ];
 
+  const isNoCreateButtonPage = noGroupCreateButtonPages.some(
+    (path) => location.pathname.startsWith(path) || location.pathname === "*"
+  );
+
   return (
     <>
       <HeaderContainer>
         <Link to="/">
           <MainLogo src={mainLogo} alt="조각집 메인 로고" />
         </Link>
-        {noGroupCreateButtonPages.some((path) =>
-          location.pathname.startsWith(path)
-        ) ? null : (
+        {!isNoCreateButtonPage && (
           <Link to="/create-group">
             <CreateGroupButton>그룹 만들기</CreateGroupButton>
           </Link>
